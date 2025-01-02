c09273ad96f2079888dd11ae2d73ae4e36ffe6a1
Tuxnix <tuxnix3@gmail.com>
2025-01-02 01:26:21 -0500

    uncommented the stage 5 functions and added grabTools to the bottom of the page as a function call

--- scripts/mpt.sh
+++ scripts/mpt.sh
@@ -224,7 +224,7 @@ createDirs(){
 installTools(){
 	echo -e '-------------------------------Step 5.1------------------------------- \n'
 	echo -e 'Installing Tools from your APT list... \n'
-	sudo apt install $(cat tools.list | tr "\n" " ") -y
+	#sudo apt install $(cat tools.list | tr "\n" " ") -y
 }
 ## Clones Repos from GitHub & installs python modules
 grabTools(){
@@ -400,5 +400,6 @@ backupFiles
 updateSys
 createDirs
 hardenSSH
-#installTools
+installTools
+grabTools
 termLog
