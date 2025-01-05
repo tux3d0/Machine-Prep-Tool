193012bacb87dae753c9e09e13c44eb00fc120f6
tux3d0 <joe.jones583@gmail.com>
2025-01-04 21:45:39 -0500

    changed Print to echo and commented out a line meant for the mpt script.

--- scripts/sysUpdate.sh
+++ scripts/sysUpdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#
-Print 'System Setup Phase #1 '
-Print 'About to update the system and bring everything current before installing tools'
+# This script is used to update the system and bring everything current before installing tools
+
+# echo 'System Setup Phase #1 '
+echo 'About to update the system and bring everything current before installing tools'
 
 echo -e 'sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'
