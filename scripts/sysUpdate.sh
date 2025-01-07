5b4d585520cffade36dfdda5a2df92ce822ad022
tux3d0 <joe.jones583@gmail.com>
2025-01-07 01:45:11 -0500

    Added a display_message() function -Changed one echo call to a display_messagge call - Added a Update complete message at the end.

--- scripts/sysUpdate.sh
+++ scripts/sysUpdate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # This script is used to update the system and bring everything current before installing tools
 
-# echo 'System Setup Phase #1 '
-echo 'About to update the system and bring everything current before installing tools'
+# Function to display a message with a border
+display_message() {
+    local message=$1
+    local border="==============================================================================="
+    echo -e "\n$border\n$message\n$border\n"
+}
+
+display_message "About to update the system and bring everything current before installing tools"
 
 echo -e 'sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'
+
+display_message "System update complete"
