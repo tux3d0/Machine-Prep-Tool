1eead54ad87a0aee8c29b14eded7cf7c8d3d05c2
tux3d0 <joe.jones583@gmail.com>
2025-01-07 01:48:46 -0500

    Added a small before a string.

--- scripts/sysUpdate.sh
+++ scripts/sysUpdate.sh
@@ -24,7 +24,7 @@ display_message() {
     echo -e "\n$border\n$message\n$border\n"
 }
 
-display_message "About to update the system and bring everything current before installing tools"
+display_message " About to update the system and bring everything current before installing tools"
 
 echo -e 'sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'
 
